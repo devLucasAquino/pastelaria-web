633763b28a30fa2a4f052cc21e29402cc7b10101
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-19 17:52:58 -0300

    style: update style home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,27 @@
+import { Link } from 'react-router-dom';
+
+import Pastel from '../assets/img/brasileiro-pastel.webp';
+
+import './Home.css';
+
 const Home = () => {
     return(
-        <h2>Home</h2>
+        <div className='home'> 
+            <header>
+                <section>
+                    <h2>Bem vindo ao website da melhor <br></br>pastelaria do Brasil, <br></br> 
+                        fique a vontade para explorar <br></br>nosso cardapio com incriveis 
+                        sabores e otimos precos!
+                    </h2>
+                    <Link to='/menu'>
+                        <button type='button'>Explorar</button>
+                    </Link>
+                </section>
+                <img src={Pastel} alt="pastel" />
+            </header>       
+            
+        </div>
+
     )
 }
 
