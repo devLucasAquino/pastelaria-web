9d994e10e1448a1a5837bb51b60bdefff8f68e40
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-21 17:41:27 -0300

    feat: tornando os itens do cardapio clicaveis

--- src/components/MenuManager.jsx
+++ src/components/MenuManager.jsx
@@ -1,17 +1,24 @@
+import { Link } from 'react-router-dom';
+
 import './MenuManager.css';
 
 const MenuManager = ({ menu }) => {
     let count = 0;
     return(
+
         <div className="list-container">
             {menu.length > 0 &&
                     menu.map((pastry) => (
-                        <div className="item-menu" key={pastry.id}>
-                            <h3>{++count} . {pastry.sabor}</h3>
-                            <h3>preco: <span> {pastry.preco} </span></h3>
-                        </div>
+                        <Link to={`/pastry?q=${pastry.sabor}`} className='link'>
+                            <div className="item-menu" key={pastry.id}>
+                                <h3>{++count} . {pastry.sabor}</h3>
+                                <h3>preco: <span> {pastry.preco} </span></h3> 
+                            </div>
+                        </Link>
                     ))}
         </div>
+        
+        
     )
 }
 
