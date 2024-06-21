f272a7d1eada12863b0aaf8c2decdbd6e7d1ee9b
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-21 17:19:05 -0300

    style: update found pastry on pastry component

--- src/pages/Pastry.jsx
+++ src/pages/Pastry.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router';
 
 import data from "../data.js";
+import onlyPastry from '../assets/img/pastryOnMenu.webp';
+
+import './Pastry.css';
 
 const Pastry = () => {
     const [ pastry, setPastry ] = useState(null);
@@ -23,14 +26,23 @@ const Pastry = () => {
         }
     }, [query]);
 
+    function sale(number){
+        const output = (number + 2).toFixed(2);
+        return output;
+    }
+
 
 
     return(
-        <div>
+        <div className='only-pastry-container'>
             {pastry ? (
-                <div>
-                    <h2>Pastel de {pastry.sabor}</h2>
-                    <h3>somente por R${pastry.preco.toFixed(2)}</h3>
+                <div className='found-pastry'>
+                    <img src={onlyPastry} alt='pastel'/>
+                    <div className='found-pastry-data'>
+                        <h2>Pastel de {pastry.sabor}</h2>
+                        <h3>de  <span id='original-value'>R${sale(pastry.preco)}  </span> 
+                            por apenas <span id='value-desconto'>R${pastry.preco.toFixed(2)}</span></h3>
+                    </div>     
                 </div>
             ) : notFound ? (
                 <h2>Pastel não encontrado</h2>
